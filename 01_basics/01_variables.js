2d5b1283060f6bbd2cb02e414f0525fc2c8aebdd
Kashish Raj <129650615+kashish9944raj@users.noreply.github.com>
2025-06-08 14:37:33 +0000

    Added 02_dataTypes.js file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "kashish@google.com"
+var accountPassword = "12345"
+accountCity = "munger"
+let accountState;
+ 
+//accountId = 2 not allowed
+console.log(accountId);
+
+ 
+accountEmail = "bla@google.com"
+accountPassword = "67891"
+accountCity = "jamui"
+ 
+//accountId = 2 not allowed
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/* prefered not to use var beacuse or issue in block issue and fucntinal issue */
